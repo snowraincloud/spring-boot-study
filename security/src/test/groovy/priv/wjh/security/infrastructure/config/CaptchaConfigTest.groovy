41b4c34194c96991f1deb3c28113bb971e3eec74
wangjunhao <1272589536@qq.com>
2022-01-19 19:56:22 +0800

    feat(security): 添加图片验证码生成功能

---
+++ security/src/test/groovy/priv/wjh/security/infrastructure/config/CaptchaConfigTest.groovy
@@ -0,0 +1,20 @@
+package priv.wjh.security.infrastructure.config
+import javax.imageio.ImageIO
+import spock.lang.Specification
+
+class CaptchaConfigTest extends Specification {
+    def captcha = new CaptchaConfig().getDefaultCaptcha()
+
+    def "test"(){
+        given:
+        def image = captcha.createImage("1254")
+        ByteArrayOutputStream tmp = new ByteArrayOutputStream()//io流
+        ImageIO.write(image, "jpg", tmp)//写入流中
+        byte[] bytes = tmp.toByteArray()//转换成字节
+
+        String png_base64 = Base64.getEncoder().encodeToString((bytes)).trim()//转换成base64串
+        png_base64 = png_base64.replaceAll("\n", "").replaceAll("\r", "")//删除 \r\n
+
+        System.out.println("值为："+"data:image/jpg;base64,"+png_base64)
+    }
+}
